b7f6c5614ecb68295bb6528eaf8d66019cd0fbfe
DareEnd <114109917+Darin1027@users.noreply.github.com>
2022-11-20 22:33:10 -0700

    added questions array to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,55 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "Input project name.",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Input project description.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Input project table of contents.",
+    name: "table",
+  },
+  {
+    type: "input",
+    message: "Input project installation instructions.",
+    name: "instructions",
+  },
+  {
+    type: "input",
+    message: "Input project usage.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Input project license.",
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "Input project contributors.",
+    name: "contributors",
+  },
+  {
+    type: "input",
+    message: "Input project tests.",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "Input project questions.",
+    name: "questions",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
